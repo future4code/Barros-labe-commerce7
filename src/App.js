c84ac203f0c7044a11f4999a2db003605e6c4dea
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2022-07-19 23:04:15 -0300

    V0 de estilização do LabCommerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {AppHeader, TituloLoja, Body, Container, Main, Linha, GradeProdutos6, Produto01,Produto02, Produto03, Produto04, Produto05, Produto06, Produtos} from "./styled";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppHeader>
+
+
+      <TituloLoja>
+        <p>TITULO DA LOJA LabCommerce</p>
+      </TituloLoja>
+      
+      <Main>MAINNN </Main>
+      
+      <Container>
+        
+      CONTAINER PRODUTOS
+      <Body>
+      BODY
+      
+      <Produtos>produtos
+        
+      <GradeProdutos6>GRADE DE PRODUTOS
+      <Linha>
+      <Produto01>IMAGEM-PRODUTO01</Produto01>
+      <Produto02>IMAGEM-PRODUTO02</Produto02>
+      <Produto03>IMAGEM-PRODUTO03</Produto03>
+      <Produto04>IMAGEM-PRODUTO04</Produto04>
+      <Produto05>IMAGEM-PRODUTO05</Produto05>
+      <Produto06>IMAGEM-PRODUTO06</Produto06>
+      </Linha>
+      </GradeProdutos6>
+      </Produtos>
+      </Body>
+      </Container>
+      
+      
+
+
+
+        HEADER
+      </AppHeader>
+      
+
+
   );
 }
 
---
+++ src/styled.js
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+
+export const AppHeader=styled.div `
+background: linear-gradient(90deg, rgba(2,0,39,1) 0%, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 1%, rgba(3,96,115,1) 100%);
+min-height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+font-size: calc(10px + 2vmin);
+color: white;
+`;
+
+export const Body=styled.div `
+margin: 0;
+font-family: "Nunito", sans-serif;
+font-size: 0.9rem;
+font-weight: 400;
+line-height: 1.6;
+color: #212529;
+text-align: left;
+background-color: #f8fafc;
+`;
+
+export const TituloLoja=styled.div `
+text-align: center;
+color: white;
+height: 60vh;
+justify-content: center;
+align-items: center;
+font-family: 'Nunito Sans', sans-serif;
+display: flex;
+flex-direction: column;
+`;
+
+export const Container=styled.div ` 
+width: 100%;
+padding-right: 15px;
+padding-left: 15px;
+margin-right: auto;
+margin-left: auto;
+`;
+
+export const Main=styled.div `
+display: block;
+`;
+
+export const Produtos=styled.div `
+padding: 5%;
+background-color: rgba(0, 126, 206, 0.1);
+text-align: center;
+display: flex;
+justify-content: center;
+flex-direction: column;
+`;
+
+export const Linha=styled.div `
+display: flex;
+flex-wrap: wrap;
+margin-right: -15px;
+margin-left: -15px;
+`;
+
+export const GradeProdutos6=styled.div `
+padding: 50px 0;
+`;
+
+export const Produto01=styled.div `
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+`;
+
+export const Produto02=styled.div `
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+`;
+
+export const Produto03=styled.div `
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+`;
+
+export const Produto04=styled.div `
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+`;
+
+export const Produto05=styled.div `
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+`;
+
+export const Produto06=styled.div `
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+`;
