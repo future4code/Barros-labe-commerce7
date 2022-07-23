0f5bea7e64af6d29ab8460e5b9a02fc8fb4eb57a
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2022-07-23 14:39:42 -0300

    V5 corrigindo conflito de git por trabalhar no mesmo arquivo App.js

---
+++ src/AppLayla.js
@@ -0,0 +1,273 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {AppHeader, Footer, FooterTitulo01, FooterItem02, FooterItem03, FooterItem04, BotaoAdicionarTexto, BotaoAdicionar, CardH3, Preço01,CardProduto01Titulo, CardProduto01, CardProduto01Interno1, CardProduto01Interno2, CardImagem01, CardProduto02, CardProduto02Interno1, CardProduto02Interno2, CardImagem02, Menu, MenuOpcoes,Home, MenuProdutos, MenuSobre, MenuContato, TituloLoja, SearchLoginCart, Search, SearchItem, Login, Cart, CartItens, CartCartao, Body, Container, Main, LinhaProduto, GradeProdutos6, Produto01,Produto02, Produto03, Produto04, Produto05, Produto06, Produtos, FooterItem01} from "./styled";
+
+//Alteração para mergear
+
+function App() {
+  return (
+    <AppHeader>
+      <Menu>
+        <MenuOpcoes></MenuOpcoes>
+          <Home>HOME</Home>
+          <MenuProdutos>PRODUTOS</MenuProdutos>
+          <MenuSobre>SOBRE</MenuSobre>
+          <MenuContato>CONTATO</MenuContato>
+        <SearchLoginCart></SearchLoginCart>
+        <Search>
+          <SearchItem>🔍 LUPA-Busca (f002)</SearchItem>
+        </Search>
+        <Cart>
+          <CartCartao>🚗Carrinho
+            <CartItens>
+              produtos adicionados?
+            </CartItens>
+          </CartCartao>
+        </Cart>
+        
+
+      </Menu>
+
+      <TituloLoja>
+        <p>TITULO DA LOJA</p>
+      </TituloLoja>
+      
+      <Main></Main>
+      
+      
+      <Container>
+        
+      
+      <Body>
+      
+      
+      <Produtos>
+        
+      <GradeProdutos6>
+      <LinhaProduto>
+
+        <CardProduto01>
+          <CardProduto01Interno1>
+            
+            <CardProduto01Interno2>
+            
+            <CardProduto01Titulo>
+              <CardH3>TÍTULO PROD01</CardH3>
+              <Preço01>R$ 100,00</Preço01>
+              
+              <BotaoAdicionar>
+              <BotaoAdicionarTexto>Adicionar</BotaoAdicionarTexto>
+              </BotaoAdicionar>
+              
+            </CardProduto01Titulo>
+              <Produto01>
+                <CardImagem01 src = "./img/mountains-g624c07561_1280.jpg"></CardImagem01> 
+              </Produto01> 
+            </CardProduto01Interno2>
+          </CardProduto01Interno1>
+        </CardProduto01>
+
+        <CardProduto01>
+          <CardProduto01Interno1>
+            
+            <CardProduto01Interno2>
+            
+            <CardProduto01Titulo>
+              <CardH3>TÍTULO PROD01</CardH3>
+              <Preço01>R$ 100,00</Preço01>
+              
+              <BotaoAdicionar>
+              <BotaoAdicionarTexto>Adicionar</BotaoAdicionarTexto>
+              </BotaoAdicionar>
+              
+            </CardProduto01Titulo>
+              <Produto01>
+                <CardImagem01 src = "./img/mountains-g624c07561_1280.jpg"></CardImagem01> 
+              </Produto01> 
+            </CardProduto01Interno2>
+          </CardProduto01Interno1>
+        </CardProduto01>
+
+        <CardProduto01>
+          <CardProduto01Interno1>
+            
+            <CardProduto01Interno2>
+            
+            <CardProduto01Titulo>
+              <CardH3>TÍTULO PROD01</CardH3>
+              <Preço01>R$ 100,00</Preço01>
+              
+              <BotaoAdicionar>
+              <BotaoAdicionarTexto>Adicionar</BotaoAdicionarTexto>
+              </BotaoAdicionar>
+              
+            </CardProduto01Titulo>
+              <Produto01>
+                <CardImagem01 src = "./img/mountains-g624c07561_1280.jpg"></CardImagem01> 
+              </Produto01> 
+            </CardProduto01Interno2>
+          </CardProduto01Interno1>
+        </CardProduto01>
+
+        <CardProduto01>
+          <CardProduto01Interno1>
+            
+            <CardProduto01Interno2>
+            
+            <CardProduto01Titulo>
+              <CardH3>TÍTULO PROD01</CardH3>
+              <Preço01>R$ 100,00</Preço01>
+              
+              <BotaoAdicionar>
+              <BotaoAdicionarTexto>Adicionar</BotaoAdicionarTexto>
+              </BotaoAdicionar>
+              
+            </CardProduto01Titulo>
+              <Produto01>
+                <CardImagem01 src = "./img/mountains-g624c07561_1280.jpg"></CardImagem01> 
+              </Produto01> 
+            </CardProduto01Interno2>
+          </CardProduto01Interno1>
+        </CardProduto01>
+
+        <CardProduto01>
+          <CardProduto01Interno1>
+            
+            <CardProduto01Interno2>
+            
+            <CardProduto01Titulo>
+              <CardH3>TÍTULO PROD01</CardH3>
+              <Preço01>R$ 100,00</Preço01>
+              
+              <BotaoAdicionar>
+              <BotaoAdicionarTexto>Adicionar</BotaoAdicionarTexto>
+              </BotaoAdicionar>
+              
+            </CardProduto01Titulo>
+              <Produto01>
+                <CardImagem01 src = "./img/mountains-g624c07561_1280.jpg"></CardImagem01> 
+              </Produto01> 
+            </CardProduto01Interno2>
+          </CardProduto01Interno1>
+        </CardProduto01>
+
+        <CardProduto01>
+          <CardProduto01Interno1>
+            
+            <CardProduto01Interno2>
+            
+            <CardProduto01Titulo>
+              <CardH3>TÍTULO PROD01</CardH3>
+              <Preço01>R$ 100,00</Preço01>
+              
+              <BotaoAdicionar>
+              <BotaoAdicionarTexto>Adicionar</BotaoAdicionarTexto>
+              </BotaoAdicionar>
+              
+            </CardProduto01Titulo>
+              <Produto01>
+                <CardImagem01 src = "./img/mountains-g624c07561_1280.jpg"></CardImagem01> 
+              </Produto01> 
+            </CardProduto01Interno2>
+          </CardProduto01Interno1>
+        </CardProduto01>
+
+        <CardProduto01>
+          <CardProduto01Interno1>
+            
+            <CardProduto01Interno2>
+            
+            <CardProduto01Titulo>
+              <CardH3>TÍTULO PROD01</CardH3>
+              <Preço01>R$ 100,00</Preço01>
+              
+              <BotaoAdicionar>
+              <BotaoAdicionarTexto>Adicionar</BotaoAdicionarTexto>
+              </BotaoAdicionar>
+              
+            </CardProduto01Titulo>
+              <Produto01>
+                <CardImagem01 src = "./img/mountains-g624c07561_1280.jpg"></CardImagem01> 
+              </Produto01> 
+            </CardProduto01Interno2>
+          </CardProduto01Interno1>
+        </CardProduto01>
+
+        <CardProduto01>
+          <CardProduto01Interno1>
+            
+            <CardProduto01Interno2>
+            
+            <CardProduto01Titulo>
+              <CardH3>TÍTULO PROD01</CardH3>
+              <Preço01>R$ 100,00</Preço01>
+              
+              <BotaoAdicionar>
+              <BotaoAdicionarTexto>Adicionar</BotaoAdicionarTexto>
+              </BotaoAdicionar>
+              
+            </CardProduto01Titulo>
+              <Produto01>
+                <CardImagem01 src = "./img/mountains-g624c07561_1280.jpg"></CardImagem01> 
+              </Produto01> 
+            </CardProduto01Interno2>
+          </CardProduto01Interno1>
+        </CardProduto01>
+      </LinhaProduto>
+
+      
+
+      </GradeProdutos6>
+      </Produtos>
+      </Body>
+      </Container>
+      
+
+      <Footer>
+        <FooterTitulo01>PRODUTOS         
+        <FooterItem01>Rochas</FooterItem01>
+         <FooterItem02>Poeiras</FooterItem02>
+         <FooterItem03>Vestimentas</FooterItem03>
+         <FooterItem04>Suvernir</FooterItem04>
+        </FooterTitulo01>
+
+
+
+        <FooterTitulo01>SERVIÇOS
+         <FooterItem01>Rochas</FooterItem01>
+         <FooterItem02>Poeiras</FooterItem02>
+         <FooterItem03>Vestimentas</FooterItem03>
+         <FooterItem04>Suvernir</FooterItem04> 
+        </FooterTitulo01>
+
+        <FooterTitulo01>CONTATO         
+        <FooterItem01>Rochas</FooterItem01>
+         <FooterItem02>Poeiras</FooterItem02>
+         <FooterItem03>Vestimentas</FooterItem03>
+         <FooterItem04>Suvernir</FooterItem04>
+        </FooterTitulo01>
+
+
+
+        <FooterTitulo01>REDES SOCIAIS
+         <FooterItem01>Rochas</FooterItem01>
+         <FooterItem02>Poeiras</FooterItem02>
+         <FooterItem03>Vestimentas</FooterItem03>
+         <FooterItem04>Suvernir</FooterItem04> 
+        </FooterTitulo01>
+
+        
+      </Footer>
+
+
+       
+      </AppHeader>
+      
+
+
+
+  );
+}
+
+export default App;
