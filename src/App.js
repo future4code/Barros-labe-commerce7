9d0d33f9d2e3b95ddef025fb9d4f317873a1ae7e
Kayo Céshar Rêgo Dias dos Santos <kayoceshar@hotmail.com>
2022-07-23 22:29:26 -0300

    alteracoes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filtro from './components/Filtro'
 import { BlocoProdutos, BotaoCarrinho } from './Styled';
 import Death from "./components/Img/death.jpg"
@@ -7,6 +7,7 @@ import Lsaber from "./components/Img/lgsaber.jpg"
 import Milano from "./components/Img/milano.jpg"
 import ViaL from "./components/Img/milkwaymap.png"
 import Planetario from "./components/Img/planetario.jpg"
+import Carrinho from './components/Carrinho';
 
 // Lista não renderizada
 
@@ -45,34 +46,76 @@ const Produtos = [
 
 ];
 
-
-  
-   
-  
-  
-
-       
-       
-  
-   
-      
-
-
-
-
-
+                
 function App() {
   // const e useStates
-  const [busca,setBusca] = useState("")
- const [minPrice,setMinPrice]= useState(-Infinity)
- const [maxPrice,setMaxPrice] = useState(Infinity)
-    const [ordenar,setOrdenar] = useState("nome")
-  const [ascender, setAscender] = useState("asc")
- 
+const [busca,setBusca] = useState("")
+const [minPrice,setMinPrice]= useState(-Infinity)
+const [maxPrice,setMaxPrice] = useState(Infinity)
+const [ordenar,setOrdenar] = useState("nome")
+const [ascender, setAscender] = useState("asc")
+const [carrinho, setCarrinho] = useState(() => {
+  if (localStorage.getItem("carrinho")) {
+    return JSON.parse(localStorage.getItem("carrinho"));
+  }
+  return [];
+})
+
+//Criando o carrinho
+useEffect(() => {
+  localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  }, [carrinho]
+)
+
+//Adicionar Produtos
+const addProdutos = (id) => {
+
+  const index = carrinho.findIndex((produto) => {
+    return produto.id === id;
+  });
+  // Adicionar o produto caso não esteja no carrinho
+  if (index < 0) {
+    const produtoAtualizado = {
+      ...Produtos.find((produto) => produto.id === id),
+      quantidade: 1,
+    };
+    const novoCarrinho = [...carrinho, produtoAtualizado];
+    setCarrinho(novoCarrinho);
+    // Caso já esteja no carrinho add +1
+  } else {
+    const novoCarrinho = carrinho.map(produto => {
+      if (produto.id === id) {
+        return { ...produto, quantidade: produto.quantidade + 1 };
+      }
+      return produto;
+    });
+    setCarrinho(novoCarrinho);
+  }
+};
+
+//remover
+
+const removerProduto = (id) => {
+  const novoCarrinho = carrinho
+    .map((produtos) => {
+      if (produtos.id === id) {
+        return { ...produtos, quantidade: produtos.quantidade - 1 };
+      }
+      return produtos;
+    })
+    .filter((produtos) => produtos.quantidade > 0);
+  setCarrinho(novoCarrinho);
+};
+
  
  
   return (
+    
     <BlocoProdutos>
+      <Carrinho
+      carrinho={carrinho}
+      removerProduto={removerProduto}           
+      />
       <Filtro
       busca={busca}
       minPrice={minPrice}
@@ -120,19 +163,23 @@ function App() {
 <img key={Produtos.image} src={val.image} alt="img" width="100"/>
  <h2 key={Produtos.nome}>{val.nome}</h2>
  <h3 key={Produtos.preco}>R$:{val.preco},00</h3>
- <BotaoCarrinho>  adicionar ao carrinho</BotaoCarrinho>
+ <BotaoCarrinho onClick={addProdutos}>  adicionar ao carrinho</BotaoCarrinho>
  </span>
        )})}
          
 
-        
+         
     
   
       
        
       
       </BlocoProdutos>
+      
+      
+ 
   )};
-
+        
+    
 
 export default App;
