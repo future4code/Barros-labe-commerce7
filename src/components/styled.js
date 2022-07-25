38a798ceaa6e4b32b51f288296e3876edc08ca1f
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2022-07-24 22:24:05 -0300

    V7 Tentativa de resolver conflito de git

---
+++ src/components/styled.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const App-header = styled.header {
+    background: linear-gradient(90deg, rgba(2,0,39,1) 0%, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 1%, rgba(3,96,115,1) 100%);
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+  
+  .Body {
+    margin: 0;
+    font-family: "Nunito", sans-serif;
+    font-size: 0.9rem;
+    font-weight: 400;
+    line-height: 1.6;
+    color: #212529;
+    text-align: left;
+    background-color: #f8fafc;  
+  }
+  
+  .titulo-loja {
+    text-align: center;
+    color: white;
+    height: 95vh;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Nunito Sans', sans-serif;
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .container {
+    width: 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  
+  .main {
+    display: block;
+  }
+  
+  .produtos {
+    padding: 5%;
+    background-color: rgba(0, 126, 206, 0.1);
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+  }
+  
+  
+  .linha {
+      display: flex;
+      flex-wrap: wrap;
+      margin-right: -15px;
+      margin-left: -15px;
+  }
+  
+  .gradeProdutos6 {
+      padding: 50px 0;
+  }
+  
+  .produto01 {
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+  
+  .produto02 {
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+  
+  .produto03 {
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+  
+  .produto04 {
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+  
+  .produto05 {
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+    padding-top: 90px; /*Adicionei gambiarra*/
+  }
+  
+  .produto06 {
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+    padding-top: 90px; /*Adicionei gambiarra*/
+  }
+  
